a2f8b2f30c4e097c8685c164c3dcd6883b4c5e93
Antonio Galea <antonio.galea@gmail.com>
2022-02-17 09:49:52 +0100

    add a pidfile

--- daemon.sh
+++ daemon.sh
@@ -2,4 +2,5 @@
 
 HERE=$(realpath $(dirname $0))
 TOMCAT8_DIR=$(find $HERE -maxdepth 1 -type d -name apache-tomcat-8\*)
+export CATALINA_PID=${TOMCAT8_DIR}/temp/tomcat.pid
 $TOMCAT8_DIR/bin/catalina.sh "$@"
