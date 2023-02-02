11cceaed9db4234f6e6944aaa288fc1cd28b32cc
Antonio Galea <antonio.galea@gmail.com>
2023-02-02 18:45:08 +0100

    limit RAM usage for Tomcat

--- daemon.sh
+++ daemon.sh
@@ -2,5 +2,6 @@
 
 HERE=$(realpath $(dirname $0))
 TOMCAT8_DIR=$HERE/apache-tomcat-8
+export CATALINA_OPTS="-Djava.awt.headless=true -server -Xms48m -Xmx512M -XX:MaxPermSize=256m"
 export CATALINA_PID=${TOMCAT8_DIR}/temp/tomcat.pid
 $TOMCAT8_DIR/bin/catalina.sh "$@"
